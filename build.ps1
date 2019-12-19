8fb96c743159f6c8b42ee52c4bb2892858330d57
Danny McCormick <damccorm@microsoft.com>
2019-12-19 14:25:09 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,14 +1,4 @@
 $ErrorActionPreference = "Stop";
 dotnet tool restore
 
-try {
-   dotnet run --project build -- $args
-}
-catch {
-  $ErrorMessage = $_.Exception.Message
-  Write-Host "Error Message"
-  Write-Host $ErrorMessage
-  $FailedItem = $_.Exception.ItemName
-  Write-Host "Failed Item"
-  Write-Host $FailedItem
-}
+dotnet run --project build -- $args
